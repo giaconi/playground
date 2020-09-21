3486da26f08ce28c56617c3302eff64306b7d20a
Iaconi Gheorghe <iaconimgheorghe@gmail.com>
2020-09-21 17:38:49 +0200

    dependencies and settings/postcss

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+          Edit
+        </button>
+        <p><code>src/App.js</code> and save to reload.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque quae officia consequuntur? Iure similique, neque at cum tempora labore rerum aliquid eligendi, repellat iusto tenetur molestias nostrum laborum illo praesentium.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
